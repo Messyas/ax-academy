73e590e14be6a794079e48698d8b285eaf6b9f41
Messyas <146555787+Messyas@users.noreply.github.com>
2026-03-02 08:51:44 -0400

    first commit

---
+++ 01-py_auto_gui/ex01.py
@@ -0,0 +1,22 @@
+# imprime a posição do mouse (várias vezes)
+
+import pyautogui
+import time
+
+QTDE = 3
+INTERVALO = 3
+
+largura, altura = pyautogui.size()
+print('Tela:', largura, 'x', altura)
+
+time.sleep(INTERVALO)
+i = 1
+x, y = pyautogui.position()
+print('Leitura', i, '-> Mouse em:', x, y)
+while i < QTDE:
+    print('Mova o mouse para outra posição em até 2s')
+    time.sleep(INTERVALO)
+    i += 1
+    x, y = pyautogui.position()
+    print('Leitura', i, '-> Mouse em:', x, y)
+print('Fim.')
